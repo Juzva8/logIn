75c2ef500663bc4c6ebeb3e5bb4d7fd773030d0e
Laurynas Juzva <laurynas.juzva@gmail.com>
2021-02-15 15:43:53 -0800

    ading auth 0

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {  Auth0Provider } from '@auth0/auth0-react'
+
+
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID; 
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider>
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById('root')
 );
 
